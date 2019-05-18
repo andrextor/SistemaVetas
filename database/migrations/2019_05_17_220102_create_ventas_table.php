d7e05d550b260fb892bc3b3566078f133ec05876
Ivan Andres <andresgom11012@gmail.com>
2019-05-18 05:25:20 -0500

    inicio ventas

--- database/migrations/2019_05_17_220102_create_ventas_table.php
+++ database/migrations/2019_05_17_220102_create_ventas_table.php
@@ -15,6 +15,16 @@ class CreateVentasTable extends Migration
     {
         Schema::create('ventas', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('cliente_id'); 
+            $table->foreign('cliente_id')->references('id')->on('perosnas'); 
+            $table->bigInteger('usuario_id'); 
+            $table->foreign('usuario_id')->references('id')->on('users'); 
+            $table->string('tipo_compobrante',20); 
+            $table->string('serie_compobrante',20)->nullable(); 
+            $table->string('num_compobrante',20); 
+            $table->decimal('impuesto',4,2); 
+            $table->decimal('total',11,2); 
+            $table->string('estado',20); 
             $table->timestamps();
         });
     }
